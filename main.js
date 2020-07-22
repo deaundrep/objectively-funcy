bc37e1e851f493ea911795b3eaaeeed28c925a41
DeAundre Prescott <deaundre.prescott@codeimmersives.com>
2020-07-22 14:32:22 -0400

    done

--- main.js
+++ main.js
@@ -1,8 +1,42 @@
 // Your code here.
+const getFirstName = function(person){
+  return person.firstName;
+}
 
+const getLastName = function(person){
+  return person.lastName;
+}
 
+const getFullName = function(person){
+  return person.firstName + ' ' + person.lastName;
+}
 
+const setFirstName = function(person ,name){
+  person.firstName = name;
+}
 
+const setAge = function(person, age){
+  person.age = age;
+}
+
+const giveBirthday = function(person){
+  person.age++;
+}
+
+const marry = function (person1, person2) {
+  person1.married = true;
+  person2.married = true;
+  person1.spouseName = person2.firstName + ' ' + person2.lastName;
+  person2.spouseName = person1.firstName + ' ' + person1.lastName;
+
+}
+
+const divorce = function(person1, person2) {
+  person1.married = false;
+  person2.married = false;
+  delete person1.spouseName;
+  delete person2.spouseName;
+}
 
 // Our code here. Don't touch!
 if (typeof getFirstName === 'undefined') {
